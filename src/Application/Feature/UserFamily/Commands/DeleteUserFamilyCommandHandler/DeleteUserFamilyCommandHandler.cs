6249d834e5dea16f8257537dc93f9bebaef44aa7
Wajji12 <wahajnadeemwp@gmail.com>
2026-02-11 16:21:14 +0500

    delete error resolved

--- src/Application/Feature/UserFamily/Commands/DeleteUserFamilyCommandHandler/DeleteUserFamilyCommandHandler.cs
+++ src/Application/Feature/UserFamily/Commands/DeleteUserFamilyCommandHandler/DeleteUserFamilyCommandHandler.cs
@@ -1,35 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using DHAFacilitationAPIs.Application.Feature.UserFamily.UserFamilyCommands.DeleteUserFamilyCommand;
+using MediatR;
+using DHAFacilitationAPIs.Application.Common.Interfaces;
+using DHAFacilitationAPIs.Domain.Entities;
 
-namespace DHAFacilitationAPIs.Application.Feature.UserFamily.UserFamilyCommands;
-
-public class DeleteUserFamilyCommandHandler
-    : IRequestHandler<DeleteUserFamilyCommand, bool>
-
+namespace DHAFacilitationAPIs.Application.Feature.UserFamily.UserFamilyCommands.DeleteUserFamily
 {
-    private readonly IApplicationDbContext _context;
-
-    public DeleteUserFamilyCommandHandler(IApplicationDbContext context)
+    public class DeleteUserFamilyCommand : IRequest<bool>
     {
-        _context = context;
+        public Guid Id { get; set; }
     }
 
-    public async Task<bool> Handle(DeleteUserFamilyCommand request, CancellationToken cancellationToken)
+    public class DeleteUserFamilyCommandHandler : IRequestHandler<DeleteUserFamilyCommand, bool>
     {
-        var entity = await _context.UserFamilies
-            .FirstOrDefaultAsync(x => x.Id == request.Id);
+        private readonly IApplicationDbContext _context;
 
-        if (entity == null)
-            return false;
+        public DeleteUserFamilyCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-        _context.UserFamilies.Remove(entity);
-        await _context.SaveChangesAsync(cancellationToken);
+        public async Task<bool> Handle(DeleteUserFamilyCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.UserFamilies
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
 
-        return true;
+            if (entity == null)
+                return false;
+
+            _context.UserFamilies.Remove(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
     }
 }
-
